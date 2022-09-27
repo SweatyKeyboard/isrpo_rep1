2c08ccb42e6bfaac6b3fba150f671776100ff46c
SweatyKeyboard <klimenkoff_v@mail.ru>
2022-09-27 17:09:41 +0300

    lab3

---
+++ ISRPO_Lab3/ISRPO_Lab3/Class1.cs
@@ -0,0 +1,21 @@
+﻿public class TestClass
+{
+    public int SomeMethod(int x)
+    {
+        return System.DateTime.Now.Millisecond % x;
+    }
+
+    public int SomeMethod2(int x)
+    {
+        TestClass2 testClass2 = new TestClass2();
+        return testClass2.SomeMehtod(x);
+    }
+}
+
+internal class TestClass2
+{
+    internal int SomeMehtod(int x)
+    {
+        return - System.DateTime.Now.Millisecond % x;
+    } 
+}
---
+++ ISRPO_Lab3_Test/ISRPO_Lab3_Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ISRPO_Lab3_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+            Console.WriteLine(testClass.SomeMethod(5));
+        }
+    }
+}
